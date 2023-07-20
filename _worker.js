704e580554dc4138f7d76a5f53d5055a9ca7b4b1
Arronlong <cclsuperstar@126.com>
2023-07-20 10:15:15 +0800

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,13 @@
+export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        //url.hostname="google.com";
+        url.hostname="discord.com" ;
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
+    }
+  };
